41d253c064a34676690417518c322f3b4cd55924
gerben-stavenga <stavenga@gmail.com>
2025-12-21 19:07:25 -0800

    Add builld.rs

---
+++ benchmark/build.rs
@@ -0,0 +1,17 @@
+// build.rs
+
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+
+    println!("cargo:rerun-if-changed=proto/test.proto");
+
+    // Generate prost version (for comparison)
+    println!("cargo:warning=Generating prost version...");
+    prost_build::Config::new()
+        .out_dir(&out_dir)
+        .compile_protos(&["../codegen/codegen-tests/proto/test.proto"], &["../codegen/codegen-tests/proto/"])?;
+
+    Ok(())
+}
